364e65bc7b0621ccd9d88c15301de72ab89cc2a6
Jawor245 <Jawor245@gmail.com>
2019-08-21 19:12:17 +0200

    Revert "usb: gadget: ether: Fix race between gether_disconnect and rx_submit"
    This reverts commit 61c0082c66cc83c9ff63395c16ddc835f56f099f.

--- drivers/usb/gadget/function/u_ether.c
+++ drivers/usb/gadget/function/u_ether.c
@@ -324,12 +324,11 @@ rx_submit(struct eth_dev *dev, struct usb_request *req, gfp_t gfp_flags)
 		out = dev->port_usb->out_ep;
 	else
 		out = NULL;
+	spin_unlock_irqrestore(&dev->lock, flags);
 
 	if (!out)
-	{
-		spin_unlock_irqrestore(&dev->lock, flags);
 		return -ENOTCONN;
-	}
+
 
 	/* Padding up to RX_EXTRA handles minor disagreements with host.
 	 * Normally we use the USB "terminate on short read" convention;
@@ -353,7 +352,6 @@ rx_submit(struct eth_dev *dev, struct usb_request *req, gfp_t gfp_flags)
 
 	if (dev->port_usb->is_fixed)
 		size = max_t(size_t, size, dev->port_usb->fixed_out_len);
-	spin_unlock_irqrestore(&dev->lock, flags);
 
 	skb = alloc_skb(size + reserve_headroom, gfp_flags);
 	if (skb == NULL) {
